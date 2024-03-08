84b6270a72abc9a86e5dd9a0e55df1a3f3bbddd2
syahirghariff <syahirghariff@gmail.com>
2024-03-08 11:28:04 +0800

    Section 7-88: moving components into seperate files

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useState } from 'react';
+import Logo from './Logo';
+import Form from './Form';
+import PackingList from './PackingList';
+import Stats from './Stats';
+
+export default function App() {
+
+  const [items, setItems] = useState([]);
+
+   function handleAddItems(item) { 
+    setItems(items => [...items, item]);
+   }
+  
+  function handleDeleteItem(id) { 
+    setItems((items) => {
+      return items.filter(item => item.id !== id);
+    })
+  }
+
+  function handleToggleItem(id) { 
+    setItems((items) => {
+      return items.map(item => item.id === id ? {...item, packed: !item.packed} : item)
+    });
+  }
+
+  function handleClearList() { 
+    const confirmed = window.confirm('Are you sure to deleate all items? '); 
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <>
+      <div className='app'>
+        <Logo />
+        <Form onAddItems={handleAddItems} /> 
+        <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearList={handleClearList} />
+        <Stats items={ items } />
+      </div>      
+    </>
+  )
+};
+
